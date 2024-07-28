dab2d1591931262c7fd2c6580bad7261386c7ee4
juyan1711 <juyan1711@gmail.com>
2024-07-28 21:19:15 +0800

    optimize: 单个用户的分组数量不超过20

--- admin/src/main/java/com/juyan/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/juyan/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,11 +1,14 @@
 package com.juyan.shortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.juyan.shortlink.admin.common.biz.user.UserContext;
+import com.juyan.shortlink.admin.common.constant.RedisCacheConstant;
+import com.juyan.shortlink.admin.common.convention.exception.ClientException;
 import com.juyan.shortlink.admin.common.convention.result.Result;
 import com.juyan.shortlink.admin.dao.entity.GroupDO;
 import com.juyan.shortlink.admin.dao.mapper.GroupMapper;
@@ -16,7 +19,11 @@ import com.juyan.shortlink.admin.remote.ShortLinkActualRemoteService;
 import com.juyan.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.juyan.shortlink.admin.service.GroupService;
 import com.juyan.shortlink.admin.toolkit.RandomGenerator;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +35,16 @@ import java.util.Optional;
  */
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+
+    /**
+     * 分布式锁 控制多端用户下分组个数不超过20个
+     */
+    private final RedissonClient redissonClient;
+
+    @Value("${short-link.group.max-num}")
+    private Integer groupMaxNum;
     ShortLinkActualRemoteService shortLinkActualRemoteService = new ShortLinkActualRemoteService() {
     };
 
@@ -39,20 +55,30 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
      */
     @Override
     public void saveGroup(String username,String groupName) {
-        String gid;
-        while (true){
-            gid = RandomGenerator.generateRandom();
-            if(hasGid(username,gid)){
-                break;
+        RLock lock = redissonClient.getLock(String.format(RedisCacheConstant.LOCK_GROUP_CREATE_KEY, username));
+        lock.lock();
+        try {
+            LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                    .eq(GroupDO::getUsername, username)
+                    .eq(GroupDO::getDelFlag, 0);
+            List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+            if (CollUtil.isNotEmpty(groupDOList) && groupDOList.size() == groupMaxNum) {
+                throw new ClientException(String.format("已超出最大分组数：%d", groupMaxNum));
             }
+            String gid;
+            do {
+                gid = RandomGenerator.generateRandom();
+            } while (!hasGid(username, gid));
+            GroupDO groupDO = GroupDO.builder()
+                    .gid(gid)
+                    .sortOrder(0)
+                    .username(username)
+                    .name(groupName)
+                    .build();
+            baseMapper.insert(groupDO);
+        } finally {
+            lock.unlock();
         }
-        GroupDO groupDO = GroupDO.builder()
-                .gid(gid)
-                .sortOrder(0)
-                .username(username)
-                .name(groupName)
-                .build();
-        int insert = baseMapper.insert(groupDO);
     }
 
     @Override
